ed8c0a81d29b3808d4f9b8e02c8bfd9139261705
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-05-07 12:49:46 +0200

    ReportService.cs class created for inherit from BackgroundService

---
+++ Rest_SikkerApi/Rest_SikkerApi/Services/ReportService.cs
@@ -0,0 +1,15 @@
+﻿using Rest_SikkerApi.models;
+using Rest_SikkerApi.repos;
+using Rest_SikkerApi.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Rest_SikkerApi.Services
+
+{
+    public class ReportService
+    {
+    }
+}
