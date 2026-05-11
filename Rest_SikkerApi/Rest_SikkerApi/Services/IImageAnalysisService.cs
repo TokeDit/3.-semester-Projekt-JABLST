8c7a1900573fb9a6c1316475332d43cc787b09f4
TokeDit <tokedit11@gmail.com>
2026-05-11 18:50:34 +0200

    VIGTIG KIG DEN RIGTIGT IGENNEM HAR SLETTET EN MASSE FILER SOM VI NOK IKKE BRUGER MEN SKAL LIGE KIGGES PAA

--- Rest_SikkerApi/Rest_SikkerApi/Services/IImageAnalysisService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Rest_SikkerApi.models;
-
-namespace Rest_SikkerApi.Services;
-
-public interface IImageAnalysisService
-{
-    Task<ImageAnalysisResult> AnalyzeAsync(IFormFile image, CancellationToken cancellationToken = default);
-}
