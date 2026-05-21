a80499847c127b5312edc2a95ee5f3055fc77ca0
lasermorph <stefanashansen@hotmail.com>
2026-05-21 10:35:21 +0200

    image amount now uses the file blob server when fetting images

--- Rest_SikkerApi/Rest_SikkerApi/Controllers/ImageController.cs
+++ Rest_SikkerApi/Rest_SikkerApi/Controllers/ImageController.cs
@@ -2,6 +2,7 @@
 using Rest_SikkerApi.repos;
 using Rest_SikkerApi.models;
 using Rest_SikkerApi.interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 namespace Rest_SikkerApi;
 
 [ApiController]
@@ -20,41 +21,59 @@ public class ImageController : ControllerBase
     // if id is provided, get images after that id, otherwise get the latest images.
     // if id is 0 it will be treated as if no id was provided, and the latest images will be returned.
     // id must be a positive integer, otherwise it will return a bad request response.
-    // [HttpGet]
-    // [ProducesResponseType (StatusCodes.Status200OK)]
-    // [ProducesResponseType (StatusCodes.Status204NoContent)]
-    // [ProducesResponseType (StatusCodes.Status404NotFound)]
-    // public async Task<IActionResult> Get([FromQuery] int? id, [FromQuery] int amount = 20)
-    // {
-    //     if (amount < 0)
-    //     {
-    //         return BadRequest("Amount must be a positive integer.");
-    //     }
-    //     else if (amount > 50)
-    //     {
-    //         amount = 50; // Limit the maximum amount to 50 to prevent excessive data retrieval
-    //     }
+    [HttpGet("images")]
+    [ProducesResponseType (StatusCodes.Status200OK)]
+    [ProducesResponseType (StatusCodes.Status204NoContent)]
+    [ProducesResponseType (StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetFixedAmount([FromQuery] int? id, [FromQuery] int amount = 20)
+    {
+        string uid = "";
+        try
+        {
+            uid = await _firebaseHandler.GetFirebaseUidAsync();
+        }
+        catch (Exception ex) when (ex is InvalidOperationException || ex is ArgumentNullException)
+        {
+            if (ex is InvalidOperationException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
 
-    //     IEnumerable<Image> images;
-    //     if (id.HasValue && id.Value >= 1)
-    //     {
-    //         images = m_repo.GetBeforeIDImage(id.Value, amount);
-    //     }
-    //     else
-    //     {
-    //         images = m_repo.GetAmountImage(amount);
-    //     }
+            if (ex is ArgumentNullException)
+            {
+                return Unauthorized("User must have a valid firebase uid");
+            }
+        }
 
-    //     if (images.Count() == 0)
-    //     {
-    //         if (id.HasValue)
-    //         {
-    //             return NotFound($"No images found with ID greater than {id.Value}.");
-    //         }
-    //         return NoContent();
-    //     }
-    //     return Ok(images);
-    // }
+        if (amount < 0)
+        {
+            return BadRequest("Amount must be a positive integer.");
+        }
+        else if (amount > 50)
+        {
+            amount = 50; // Limit the maximum amount to 50 to prevent excessive data retrieval
+        }
+
+        IEnumerable<Image> images;
+        if (id.HasValue && id.Value >= 1)
+        {
+            images = await m_repo.GetBeforeIDImageAsync(uid, id.Value, amount);
+        }
+        else
+        {
+            images = m_repo.GetAmountImage(uid, amount);
+        }
+
+        if (images.Count() == 0)
+        {
+            if (id.HasValue)
+            {
+                return NotFound($"No images found with ID greater than {id.Value}.");
+            }
+            return NoContent();
+        }
+        return Ok(images);
+    }
 
     [HttpGet]
     [ProducesResponseType (StatusCodes.Status200OK)]
--- Rest_SikkerApi/Rest_SikkerApi/repos/ISikkerRepo.cs
+++ Rest_SikkerApi/Rest_SikkerApi/repos/ISikkerRepo.cs
@@ -18,8 +18,9 @@ namespace Rest_SikkerApi.repos
         //ADDITIONAL METHODS FOR USER MANAGEMENT
         Task<List<Image>> GetImagesByOwnerUidAndMonthAsync(string ownerUid, int year, int month);
         Task<User?> UpdateUserAsync(string ownerUid, string? telegramChatId, int reportFrequency, bool reportEnabled);
-        IEnumerable<Image> GetAmountImage(int amount = 20);
+        IEnumerable<Image> GetAmountImage(string uid, int amount = 20);
         IEnumerable<Image> GetAfterIDImage(int id, int amount = 20);
+        Task<IEnumerable<Image>> GetBeforeIDImageAsync(string uid, int id, int amount = 20);
 
         bool GetSystemState();
         bool SetSystemState(bool state);
